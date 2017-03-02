0e43404c9f22f718252aa1e46db64a37dcb8d238
dennis <denniszl14@gmail.com>
2017-03-01 21:58:36 -0600

    fix some html, add a period. make it actually work.

--- theNextBigThing/makeBig.py
+++ theNextBigThing/makeBig.py
@@ -1,7 +1,7 @@
 def makeBig(thingToMakeBig):
     if thingToMakeBig:
         bigThing = 'e' + thingToMakeBig.capitalize()
-        bigString = 'The next big thing is {}'.format(bigThing)
+        bigString = 'The next big thing is {}.'.format(bigThing)
     else:
         bigString = 'Give me something to start with, man.'
     return bigString

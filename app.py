edd53880baf47dbcb6b8cac0fd39ba9bf3f2d184
dennis <denniszl14@gmail.com>
2017-04-20 19:26:52 -0500

    Remove a debugging message

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def main():
     if request.method == 'GET':
         return render_template('big.html')
     elif request.method == 'POST':
-        print request.form
         return makeBig(request.form['makeMeBig'])
 
 if __name__ == '__main__':
